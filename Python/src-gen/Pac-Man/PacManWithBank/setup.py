23456c9d0f6f0488756413a2e07b72d6d6c39fb9
Soroush Bateni <soroosh129@gmail.com>
2022-06-10 09:46:46 -0500

    Removed src-gen directory

--- Python/src-gen/Pac-Man/PacManWithBank/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, Extension
-
-linguafrancaPacManWithBankmodule = Extension("LinguaFrancaPacManWithBank",
-                                            sources = ["ctarget/schedule.c", "ctarget/util.c", "core/mixed_radix.c", "core/platform/lf_linux_support.c", "PacManWithBank.c"],
-                                            define_macros=[("MODULE_NAME", "LinguaFrancaPacManWithBank"), ("LOG_LEVEL", "2")])
-
-setup(name="LinguaFrancaPacManWithBank", version="1.0",
-        ext_modules = [linguafrancaPacManWithBankmodule],
-        install_requires=["LinguaFrancaBase"])
