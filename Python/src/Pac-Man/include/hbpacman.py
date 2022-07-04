9c8dc239eb3a60fb718ff428d39f7fa6f635f346
Benjamin Asch <benjamintasch@berkeley.edu>
2022-07-03 22:45:23 -0500

    made appearance changes to foomba

--- Python/src/Pac-Man/include/hbpacman.py
+++ Python/src/Pac-Man/include/hbpacman.py
@@ -10,9 +10,11 @@ black = (0,0,0)
 white = (255,255,255)
 blue = (0,0,255)
 green = (0,255,0)
+gray = (90, 90, 90)
 red = (255,0,0)
 purple = (255,0,255)
 yellow   = (255, 255, 0)
+brown = (139, 69, 19)
 walls = [ [0,0,6,600],
               [0,0,600,6],
               [0,600,606,6],
@@ -118,6 +120,60 @@ def setupRoomOne(all_sprites_list):
     # Loop through the list. Create the wall, add it to the list
     for item in walls:
         wall=Wall(item[0],item[1],item[2],item[3],blue)
+        wall_list.add(wall)
+        all_sprites_list.add(wall)
+         
+    # return our new list
+    return wall_list
+
+def setupRoomFoomba(all_sprites_list):
+    # Make the walls. (x_pos, y_pos, width, height)
+    wall_list=pygame.sprite.RenderPlain()
+     
+    # This is a list of walls. Each is in the form [x, y, width, height]
+    walls = [ [0,0,6,600],
+              [0,0,600,6],
+              [0,600,606,6],
+              [600,0,6,606],
+              [300,0,6,66],
+              [60,60,186,6],
+              [360,60,186,6],
+              [60,120,66,6],
+              [60,120,6,126],
+              [180,120,246,6],
+              [300,120,6,66],
+              [480,120,66,6],
+              [540,120,6,126],
+              [120,180,126,6],
+              [120,180,6,126],
+              [360,180,126,6],
+              [480,180,6,126],
+              [180,240,6,126],
+              [180,360,246,6],
+              [420,240,6,126],
+              [240,240,42,6],
+              [324,240,42,6],
+              [240,240,6,66],
+              [240,300,126,6],
+              [360,240,6,66],
+              [0,300,66,6],
+              [540,300,66,6],
+              [60,360,66,6],
+              [60,360,6,186],
+              [480,360,66,6],
+              [540,360,6,186],
+              [120,420,366,6],
+              [120,420,6,66],
+              [480,420,6,66],
+              [180,480,246,6],
+              [300,480,6,66],
+              [120,540,126,6],
+              [360,540,126,6]
+            ]
+     
+    # Loop through the list. Create the wall, add it to the list
+    for item in walls:
+        wall=Wall(item[0],item[1],item[2],item[3], gray)
         wall_list.add(wall)
         all_sprites_list.add(wall)
          
