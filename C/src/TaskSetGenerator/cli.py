62d8b0e862aa4426d00a2b549f6555928be06d38
Wonseo-C <1202won@gmail.com>
2022-08-22 17:35:09 +0900

    Make scheduler args list

--- C/src/TaskSetGenerator/cli.py
+++ C/src/TaskSetGenerator/cli.py
@@ -19,15 +19,15 @@ class CLI(object):
 
         parser = argparse.ArgumentParser(description="A GUI program for task set generator. (Command line interface version.)")
         # Set schdeulers
-        parser.add_argument("-S", "--schedulers", type=str, required=True,
+        parser.add_argument("-S", "--schedulers",  nargs='+', type=str, required=True,
                             help="Choose the schedulers: 'NP', 'GEDF_NP', 'GEDF_NP_CI, 'adaptive'")
         # Set General Configuration
         parser.add_argument("-NI", "--num_iteration", type=int, required=True,
                             help="Set the number of iterations")
-        parser.add_argument("-D", "--deadline", type=str, required=True, default="1_sec",
+        parser.add_argument("-D", "--deadline", nargs='+', type=str, required=True, default=["1", "sec"],
                             help="Set the dealine(ex. 1sec); can choose the unit(sec, msec, usec, nsec)")
-        parser.add_argument("-TT", "--total_time", type=str, default="1_sec",
-                            help="Set the total time(ex. 1sec); can choose the unit(sec, msec, usec, nsec)")
+        parser.add_argument("-TT", "--total_time", nargs='+', type=str, default=["1", "sec"],
+                            help="Set the total time(ex. 1 sec); can choose the unit(sec, msec, usec, nsec)")
         
         # Choose the type of task
         parser.add_argument("-T", "--type", type=str, required=True,
@@ -40,16 +40,16 @@ class CLI(object):
                             help="Set the number of tasks")
         parser.add_argument("-U", "--utilization", type=float,
                             help="Set the utilization")
-        parser.add_argument("--period", type=str, default="1_sec",
-                            help="Set the period(ex. 1sec); can choose the unit(sec, msec, usec, nsec)")
+        parser.add_argument("--period", type=str, nargs='+', default=["1", "sec"],
+                            help="Set the period(ex. 1 sec); can choose the unit(sec, msec, usec, nsec)")
         
         # DAG Taskset
         parser.add_argument("-NL", "--num_level", type=int,
                             help="Choose the number of level")
         parser.add_argument("-MNC", "--max_num_components", type=int,
                             help="Set the maximum number of components in one level")
-        parser.add_argument("-TC", "--components_time", type=str, default="1_sec",
-                            help="Set the exectuion time of each component(ex. 1sec); can choose the unit(sec, msec, usec, nsec)")
+        parser.add_argument("-TC", "--components_time", nargs='+', type=str, default=["1", "sec"],
+                            help="Set the exectuion time of each component(ex. 1 sec); can choose the unit(sec, msec, usec, nsec)")
         
         # Optional setting random seed
         parser.add_argument("--seed", type=int, default=1234,
@@ -104,15 +104,15 @@ class CLI(object):
 
     def setConfig(self):
 
-        # self.taskConfig['schedulers'] = self.args.schedulers
-        self.taskConfig['schedulers'] = ["NP"]
+        self.taskConfig['schedulers'] = self.args.schedulers
+        # self.taskConfig['schedulers'] = ["NP"]
         
             
         self.taskConfig['type'] = self.args.type
 
         self.taskConfig['timeout'] = {
-            'value': int(self.args.total_time.split("_")[0]),
-            'timeUnit': self.args.total_time.split("_")[1]
+            'value': int(self.args.total_time[0]),
+            'timeUnit': self.args.total_time[1]
         }
 
         # FIXME: Should add spinboxs for min_workers and max_workers in GUI.
@@ -120,15 +120,15 @@ class CLI(object):
         self.taskConfig['max_workers'] = 20
 
         self.taskConfig['deadline'] = {
-            'value': int(self.args.deadline.split("_")[0]),
-            'timeUnit': self.args.deadline.split("_")[1]
+            'value': int(self.args.deadline[0]),
+            'timeUnit': self.args.deadline[1]
         }
 
         if self.taskConfig['type'] == 'basic':
             self.taskConfig['periodicity'] = self.args.periodicity
             self.taskConfig['period'] = {
-               'value': int(self.args.period.split("_")[0]),
-               'timeUnit': self.args.period.split("_")[1]
+               'value': int(self.args.period[0]),
+               'timeUnit': self.args.period[1]
             }
             self.taskConfig['num_tasks'] = self.args.num_tasks
             self.taskConfig['utilization'] = self.args.utilization
@@ -139,15 +139,15 @@ class CLI(object):
             self.taskConfig['max_depth'] = self.args.max_depth
             self.taskConfig['num_outputs'] = self.args.num_outputs
             self.taskConfig['execution_time'] = {
-               'value': int(self.args.execution_time.split("_")[0]),
-               'timeUnit': self.args.execution_time.split("_")[1]
+               'value': int(self.args.execution_time[0]),
+               'timeUnit': self.args.execution_time[1]
             }
 
     def saveResult(self, result, output_dir):
         
         if self.taskConfig['type'] == 'basic':
             header = ['type', 'number of iterations', 'deadline', 'schedulers', 'number of tasks', 'total time', 'utilization', 'periodicity']
-            configs = ['basic', self.spinBox_numOfIterations.value(), f'{self.taskConfig["deadline"]["value"]} {self.taskConfig["deadline"]["timeUnit"]}', 
+            configs = ['basic', self.args.num_iteration, f'{self.taskConfig["deadline"]["value"]} {self.taskConfig["deadline"]["timeUnit"]}', 
                         ', '.join(self.taskConfig['schedulers']), self.taskConfig['num_tasks'], f'{self.taskConfig["timeout"]["value"]} {self.taskConfig["timeout"]["timeUnit"]}',
                         self.taskConfig['utilization'], self.taskConfig['periodicity']
                       ]
@@ -160,7 +160,7 @@ class CLI(object):
 
         elif self.taskConfig['type'] == 'dag':
             header = ['type', 'number of iterations', 'deadline', 'schedulers', 'number of level', 'maximum components in each level', 'seed', 'execution time']
-            configs = ['dag', self.spinBox_numOfIterations.value(), f'{self.taskConfig["deadline"]["value"]} {self.taskConfig["deadline"]["timeUnit"]}', 
+            configs = ['dag', self.args.num_iteration, f'{self.taskConfig["deadline"]["value"]} {self.taskConfig["deadline"]["timeUnit"]}', 
                         ', '.join(self.taskConfig['schedulers']), self.taskConfig['max_depth'], self.taskConfig['num_outputs'], self.taskConfig['seed'],
                         f'{self.taskConfig["execution_time"]["value"]} {self.taskConfig["execution_time"]["timeUnit"]}'
                       ]
