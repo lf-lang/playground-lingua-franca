81c2bc3d7197b98b42f2269c34b4d2f88a470959
Dongha Kim <dongha@asu.edu>
2025-02-25 15:12:33 -0700

    Add compute_natural_failure()

--- examples/C/src/Re-execution.c
+++ examples/C/src/Re-execution.c
@@ -7,6 +7,22 @@ double compute_lambda_per_cycle(double lambda_per_hour, double k) {
     return -log(1 - lambda_per_hour) / k; 
 }
 
+// Compute the natural failure probability for a task
+double compute_natural_failure(double lambda_cpu, double lambda_mem, int exposure_cpu, int exposure_mem) {
+    // double p_F_cpu = 1 - pow(1 - lambda_cpu, exposure_cpu); 
+    // The original equation above does not work because too small values.
+    // Same with below mathematically.
+    // (1-λ)^k ≈ e^(-λk) for small λ (approximation).
+    double p_F_cpu = 1 - exp(-lambda_cpu * exposure_cpu); // Failure probability from cpu faults.
+
+    // double p_F_cpu = 1 - pow(1 - lambda_cpu, exposure_cpu); // Same with below.
+    double p_F_mem = 1 - exp(-lambda_mem * exposure_mem); // Failure probability from memory faults.
+    
+    // Combined probability of failure.
+    // 1 - Π (1 - λ')^(ε_(i, resource)
+    return 1 - (1 - p_F_cpu) * (1 - p_F_mem); 
+}
+
 int main() {
     // Given values
     double lambda_cpu_per_hour = 1e-6; // λ_cpu: CPU fault rate per hour.
@@ -22,6 +38,11 @@ int main() {
     // Get λ' to compute the natural failure.
     // λ'_cpu
     double lambda_cpu_per_cycle = compute_lambda_per_cycle(lambda_cpu_per_hour, k); 
+    // λ'_mem
+    double lambda_mem_per_cycle = compute_lambda_per_cycle(lambda_mem_per_hour, k);
+
+    // Get natural failure rate p_F.
+    double p_F = compute_natural_failure(lambda_cpu_per_cycle, lambda_mem_per_cycle, exposure_cpu, exposure_mem);
 
     return 0;
 }
