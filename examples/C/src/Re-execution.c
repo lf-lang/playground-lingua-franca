4775857ba24e2d057cb3d683181e631b8e331542
Dongha Kim <dongha@asu.edu>
2025-02-25 13:03:29 -0700

    Add compute_lambda_per_cycle()

--- examples/C/src/Re-execution.c
+++ examples/C/src/Re-execution.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
+// Get the per-cycle fault probability λ' = 1 - (1 -λ)^(1/k)
+double compute_lambda_per_cycle(double lambda_per_hour, double k) {
+    // return 1 - pow(1 - lambda_per_hour, 1.0 / k); // Same with below.
+    return -log(1 - lambda_per_hour) / k; 
+}
+
 int main() {
     // Given values
     double lambda_cpu_per_hour = 1e-6; // λ_cpu: CPU fault rate per hour.
@@ -11,6 +17,11 @@ int main() {
     int exposure_mem = 10000;  // ε_(i, mem): Memory exposure time in cycles (full period).
 
     // double p_req = 1e-9; // Required failure rate per hour. Gets 0 re-execution.
-     double p_req = 1e-15; // Stricter failure probability requirement. Gets 1 re-execution.
+    double p_req = 1e-15; // Stricter failure probability requirement. Gets 1 re-execution.
+
+    // Get λ' to compute the natural failure.
+    // λ'_cpu
+    double lambda_cpu_per_cycle = compute_lambda_per_cycle(lambda_cpu_per_hour, k); 
+
     return 0;
 }
