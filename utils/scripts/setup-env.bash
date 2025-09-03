14bdc735a78b98eb47ee100bbc6e806748a66413
Dongha Kim <dongha@asu.edu>
2025-09-02 17:29:38 -0700

    Test CI.

--- utils/scripts/setup-env.bash
+++ utils/scripts/setup-env.bash
@@ -60,6 +60,7 @@ if [ $SETUP_NETWORK = true ]; then
     cmake ..
     make
     popd
+    # test
     # Install crow
     curl --proto '=https' --tlsv1.2 -L -o ./crow.deb "${CROW_URL}"
     sudo apt-get install --assume-yes ./crow.deb
