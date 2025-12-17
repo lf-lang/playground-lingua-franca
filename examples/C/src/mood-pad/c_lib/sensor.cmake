e9cdc9f3edf68263b2c5a356572241fff86f1ed4
Dongha Kim <74869052+Jakio815@users.noreply.github.com>
2025-12-17 14:15:21 -0700

    Update examples/C/src/mood-pad/c_lib/sensor.cmake

--- examples/C/src/mood-pad/c_lib/sensor.cmake
+++ examples/C/src/mood-pad/c_lib/sensor.cmake
@@ -1,28 +1,9 @@
-file(GLOB_RECURSE BME280_SRC
-    "${CMAKE_CURRENT_SOURCE_DIR}/BME280_SensorAPI/*.c"
-    "${CMAKE_CURRENT_SOURCE_DIR}/BME280_SensorAPI/*.h"
-)
-
-foreach(FILE_PATH ${BME280_SRC})
-    if(FILE_PATH MATCHES ".*/BME280_SensorAPI/examples/.*")
-        list(REMOVE_ITEM BME280_SRC ${FILE_PATH})
-    endif()
-endforeach()
-
 target_sources(${LF_MAIN_TARGET} PRIVATE 
     temp_utils.c
-    temp_utils.h
-    ${BME280_SRC}
-    light_utils.h
     light_utils.c
-    light_utils.h
     capacitive_utils.c
-)
-
-target_include_directories(${LF_MAIN_TARGET} PRIVATE
-    ${CMAKE_CURRENT_SOURCE_DIR}/BME280_SensorAPI
-    /usr/include
-
+    bme280.c
 )
 
 target_link_libraries(${LF_MAIN_TARGET} PRIVATE gpiod fluidsynth)
+
