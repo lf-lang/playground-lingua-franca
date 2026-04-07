fff3de6e5c2dd99a9634dd0e74a44fb37d0c0c61
IreneMarciana <irenerfahndrich@gmail.com>
2026-04-07 16:34:18 -0700

    functional loadcell reactor + init readme

--- examples/C/src/automated-cat-feeder/README.md
+++ examples/C/src/automated-cat-feeder/README.md
@@ -1,1 +1,38 @@
-Automated Cat Feeder using Lingua Franca(C target)
+<h1 align="center">
+  <br>
+  Automated Cat Feeder
+  <br>
+</h1>
+
+## Overview
+placeholder
+
+## Components
+<table>
+  <tr>
+    <td> <a href="lib/motor.lf">motor.lf</a>: This is the reactor for the stepper motor, using modal reactors for modes IDLE, DISPENSE, and ERROR.</td>
+  </tr>
+  <tr>
+    <td> <a href="lib/loadcell.lf">loadcell.lf</a>: This reactor handles the loadcell reaction, which is triggered every 1 second and takes 10 samples. It outputs a boolean determining whether the food bowl is above or below a certain threshold.</td>
+  </tr>
+  <tr>
+    <td> <a href="feed.lf">feed.lf</a>: This is the main reactor, combining loadcell measurement and motor modal logic for feeding process.</td>
+  </tr>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+To be done:
+- overview
+- hardware
+- software(mod bcm + hx711 changes)
+- demo
+- usage
--- examples/C/src/automated-cat-feeder/c_lib/hx711/hx711.c
+++ examples/C/src/automated-cat-feeder/c_lib/hx711/hx711.c
@@ -1,23 +1,31 @@
 #include "hx711.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+// changed!
+// added memory barrier
+#define MEM_BARRIER() __sync_synchronize()
 
 int setupGPIO(HX711 *hx)
 {
   setup_io();
+
   INP_GPIO(hx->data_pin);
   INP_GPIO(hx->clock_pin);
   OUT_GPIO(hx->clock_pin);
-  int err = setPinState(hx->clock_pin, false);
-  if (err)
-  {
-    printf("\n!!! Failed to set pin %d\n", hx->clock_pin);
-    return 1;
-  }
+
+  // changed! 
+  //ensure clock begins low instead of error msg
+  setPinState(hx->clock_pin, 0);
+  usleep(10);
+
   return 0;
 }
 
 void cleanGPIO(HX711 *hx)
 {
-  // unpull pins
+  //unpull pins
   GPIO_PULL = 0;
   GPIO_PULLCLK0 = 1 << hx->data_pin;
   GPIO_PULL = 0;
@@ -25,11 +33,10 @@ void cleanGPIO(HX711 *hx)
   restore_io();
 }
 
-// set higher priority for the process
+//set higher priority for the process
 int setPriority(int priority)
 {
   struct sched_param sched;
-
   memset(&sched, 0, sizeof(sched));
 
   sched.sched_priority = priority;
@@ -40,237 +47,184 @@ int setPriority(int priority)
   return 0;
 }
 
+
 void reset(HX711 *hx)
 {
-  setPinState(hx->clock_pin, true);
-  usleep(60);
-  setPinState(hx->clock_pin, false);
-  usleep(60);
+  setPinState(hx->clock_pin, 1);
+  usleep(100);
+  setPinState(hx->clock_pin, 0);
+  usleep(100);
 }
+
 
 void setGain(HX711 *hx)
 {
-  // 1 is 128 gain channel a
-  // 2 is 32  gain channel b
-  // 3 is 63  gain channel a
-  int x = 0;
-  if (hx->wanted_channel == 'A' && hx->gain_channel_A == 128)
-  {
-    x = 1;
-  }
-  else if (hx->wanted_channel == 'B')
-  {
-    x = 2;
-  }
-  else
-  {
-    x = 3;
-  }
+  // changed!
+  int pulses = 1;
 
-  for (int i = 0; i < x; ++i)
+  if (hx->wanted_channel == 'B')
+    pulses = 2;
+  else if (hx->gain_channel_A == 64)
+    pulses = 3;
+
+  for (int i = 0; i < pulses; i++)
   {
-    setPinState(hx->clock_pin, true);
-    for (int i = 0; i < 4; ++i)
-    {
-      continue;
-    }
-    setPinState(hx->clock_pin, false);
-    for (int i = 0; i < 4; ++i)
-    {
-      continue;
-    }
+    setPinState(hx->clock_pin, 1);
+    usleep(1);
+    setPinState(hx->clock_pin, 0);
+    usleep(1);
   }
 }
 
+// changed!
 int getRawData(HX711 *hx)
 {
   unsigned int bits = 0;
 
+  // wait until low data
   while (getPinState(hx->data_pin))
   {
-    usleep(10000);
-  };
-  for (int i = 0; i < 4; ++i)
-  {
-    continue;
+    usleep(100);
   }
 
-  for (int i = 0; i < 24; ++i)
+  // read 24 bits of data
+  for (int i = 0; i < 24; i++)
   {
-    setPinState(hx->clock_pin, true);
-    for (int i = 0; i < 4; ++i)
-    {
-      continue;
-    }
-    setPinState(hx->clock_pin, false);
-    for (int i = 0; i < 2; ++i)
-    {
-      continue;
-    }
-    bits = bits << 1 | (getPinState(hx->data_pin) > 0);
+    setPinState(hx->clock_pin, 1);
+    usleep(1);
+
+    bits = (bits << 1) | (getPinState(hx->data_pin) ? 1 : 0);
+
+    setPinState(hx->clock_pin, 0);
+    usleep(1);
   }
 
+  // set gain for next read
   setGain(hx);
 
   //  bits = ~0x1800000 & bits;
   //  bits = ~0x800000 & bits;
-
   if (bits & 0x800000)
   {
-    bits |= ~0xffffff;
+    bits |= ~0xFFFFFF;
   }
 
-  return bits;
+  return (int)bits;
 }
 
+
+// changed!
+// memory barrier!!
 bool getPinState(unsigned pin_number)
 {
   if (pin_number > 31)
   {
-    printf("getPinState - wrong pin number: %d", pin_number);
+    printf("getPinState - wrong pin number: %d\n", pin_number);
     return false;
   }
-  return (*(gpio + 13) & (1 << pin_number));
+
+  MEM_BARRIER();
+
+  int value = (*(gpio + 13) & (1 << pin_number));
+
+  MEM_BARRIER();
+
+  return value != 0;
 }
 
+// changed!
+// memory barrier!!
 int setPinState(unsigned pin_number, bool state)
 {
   if (pin_number > 31)
   {
-    printf("setPinState - wrong pin number: %d", pin_number);
+    printf("setPinState - wrong pin number: %d\n", pin_number);
     return 1;
   }
+
+  MEM_BARRIER();
+
   if (state)
-  {
     *(gpio + 7) = (1 << pin_number);
-  }
   else
-  {
     *(gpio + 10) = (1 << pin_number);
-  }
+
+  MEM_BARRIER();
+
   return 0;
 }
 
+
 int initHX711(HX711 *hx, unsigned char clock_pin, unsigned char data_pin)
 {
-  if (clock_pin > 31)
+  if (clock_pin > 31 || data_pin > 31)
   {
-    printf("\nWrong clock_pin number: %d", clock_pin);
+    printf("Invalid GPIO pins\n");
     return 1;
   }
-  else if (data_pin > 31)
-  {
-    printf("\nWrong data_pin number: %d", data_pin);
-    return 1;
-  }
+
   hx->clock_pin = clock_pin;
   hx->data_pin = data_pin;
   hx->gain_channel_A = 128;
   hx->current_channel = 'A';
   hx->wanted_channel = 'A';
+
   hx->offset_A_128 = 0;
   hx->offset_A_64 = 0;
   hx->offset_B = 0;
+
   hx->scale_ratio_A_128 = 0.0;
   hx->scale_ratio_A_64 = 0.0;
   hx->scale_ratio_B = 0.0;
+
   hx->filterPtr = NULL;
+
   return 0;
 }
 
+//changed!
 int zeroScale(HX711 *hx)
 {
-  double result = getRawDataMean(hx, 160);
+  double result = getRawDataMean(hx, 50);
+
   if (hx->current_channel == 'A' && hx->gain_channel_A == 128)
   {
     hx->offset_A_128 = result;
     return 0;
   }
-  else if (hx->current_channel == 'A' && hx->gain_channel_A == 64)
-  {
-    hx->offset_A_64 = result;
-    return 0;
-  }
-  else if (hx->current_channel == 'B')
-  {
-    hx->offset_B = result;
-    return 0;
-  }
-  printf("\n\n!!!zeroScale - current channel is wrong: %c\n", hx->current_channel);
+
   return 1;
 }
 
+// changed!
 int getRawDataMean(HX711 *hx, int samples)
 {
-  int *data;
-  data = malloc(samples * sizeof(int));
-
-  for (int i = 0; i < samples; ++i)
-  {
-    data[i] = getRawData(hx);
-    usleep(10000);
-  }
   long sum = 0;
-  if (hx->filterPtr != NULL)
-  {
-    int *filtered_data;
-    filtered_data = hx->filterPtr(data, samples);
-    free(data);
-    int i = 0;
-    while (i < samples && filtered_data[i] != 0)
-    {
-      sum += filtered_data[i];
-      ++i;
-    }
-    free(filtered_data);
 
-    return (int)(sum / i);
-  }
-  else
+  for (int i = 0; i < samples; i++)
   {
-    for (int i = 0; i < samples; ++i)
-    {
-      sum += data[i];
-    }
-    free(data);
-    return (int)(sum / samples);
+    int val = getRawData(hx);
+    sum += val;
+    usleep(5000);
   }
+
+  return (int)(sum / samples);
 }
 
+// changed!
 int getDataMean(HX711 *hx, int samples)
 {
   int result = getRawDataMean(hx, samples);
-  if (hx->current_channel == 'A' && hx->gain_channel_A == 128)
-  {
-    return result - hx->offset_A_128;
-  }
-  else if (hx->current_channel == 'A' && hx->gain_channel_A == 64)
-  {
-    return result - hx->offset_A_64;
-  }
-  else if (hx->current_channel == 'B')
-  {
-    return result - hx->offset_B;
-  }
-  printf("\n\n!!!getDataMean - current channel is wron: %c\n", hx->current_channel);
-  return 1;
+  return result - hx->offset_A_128;
 }
 
+// changed!
 int getWeightMean(HX711 *hx, int samples)
 {
-  double result = getRawDataMean(hx, samples);
-  if (hx->current_channel == 'A' && hx->gain_channel_A == 128)
-  {
-    return (int)((result - hx->offset_A_128) / hx->scale_ratio_A_128);
-  }
-  else if (hx->current_channel == 'A' && hx->gain_channel_A == 64)
-  {
-    return (int)((result - hx->offset_A_64) / hx->scale_ratio_A_64);
-  }
-  else if (hx->current_channel == 'B')
-  {
-    return (int)((result - hx->offset_B) / hx->scale_ratio_B);
-  }
-  printf("\n\n!!!getWeightMean - current channel is wron: %c\n", hx->current_channel);
-  return 1;
+  int result = getRawDataMean(hx, samples);
+
+  if (hx->scale_ratio_A_128 == 0)
+    return 0;
+
+  return (int)((result - hx->offset_A_128) / hx->scale_ratio_A_128);
 }
